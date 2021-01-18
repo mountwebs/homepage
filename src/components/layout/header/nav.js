6c09c3afaa60a2dac56b141b79e19e03b673cb06
mountwebs <mountwebs@gmail.com>
2021-01-18 12:21:42 +0100

    added icons in nav

--- src/components/layout/header/nav.js
+++ src/components/layout/header/nav.js
@@ -2,6 +2,16 @@ import React from "react";
 import { css } from "@emotion/react";
 import { Link } from "gatsby";
 import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { VscGithub } from "react-icons/vsc";
+
+const containerCss = css`
+  display: flex;
+`;
+
+const navCss = css`
+  // line-height: 29px;
+`;
 
 const linkCss = css`
   text-decoration: none;
@@ -13,18 +23,49 @@ const linkCss = css`
   }
 `;
 
+const iconContainerCss = css`
+  margin-left: 10px;
+  display: flex;
+  align-icons: flex-end;
+`;
+
 const Nav = () => {
   return (
-    <div>
-      <AnchorLink css={linkCss} to="/#aboutMe">
-        about
-      </AnchorLink>
-      <AnchorLink css={linkCss} to="/#projects">
-        projects
-      </AnchorLink>
-      <Link css={linkCss} to={`/blog/`} activeStyle={{ color: "red" }}>
-        blog
-      </Link>
+    <div css={containerCss}>
+      <div css={navCss}>
+        <AnchorLink css={linkCss} to="/#aboutMe">
+          about
+        </AnchorLink>
+        <AnchorLink css={linkCss} to="/#projects">
+          projects
+        </AnchorLink>
+        <Link css={linkCss} to={`/blog/`} activeStyle={{ color: "red" }}>
+          blog
+        </Link>
+      </div>
+      <div css={iconContainerCss}>
+        <a
+          href="https://www.linkedin.com/in/stian-klasbu-917a40162/"
+          style={{ color: "inherit", display: "flex", alignItems: "flex-end" }}
+        >
+          {" "}
+          <AiOutlineLinkedin style={{ height: 24, width: 24 }} />
+        </a>
+
+        <a
+          href="https://github.com/mountwebs"
+          style={{ color: "inherit", display: "flex", alignItems: "flex-end" }}
+        >
+          <VscGithub
+            style={{
+              height: 23,
+              width: 23,
+              padding: "0px 0 2px",
+              marginLeft: 2,
+            }}
+          />
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/main/aboutMe.js
+++ src/components/main/aboutMe.js
@@ -38,7 +38,7 @@ const AboutMe = () => {
         </p>
         <p>
           In my spare time I love to create things - whether that is to create
-          an IoT solution for my smart home, create som music with my friends,
+          an IoT solution for my smart home, create music with some friends,
           create generative art with code, or create an app.
         </p>
       </article>
