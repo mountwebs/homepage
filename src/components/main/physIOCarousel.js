0b53a8bb53ec3999fedd19af51f429599b469250
mountwebs <mountwebs@gmail.com>
2021-01-19 19:03:40 +0100

    added physio carousel

--- src/components/main/physIOCarousel.js
+++ src/components/main/physIOCarousel.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { css } from "@emotion/react";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function importAll(r) {
   let images = {};
@@ -14,35 +17,115 @@ const images = importAll(
   require.context("../../assets/images/physIO", false, /.png/)
 );
 
+const imageCss = css`
+  max-height: 400px;
+  width: auto;
+  margin: 0 auto;
+`;
+
+const contentArray = [
+  ["about.png"],
+  ["client-dashboard.png"],
+  ["client-session.png"],
+  ["homepage.png"],
+  ["login.png"],
+  ["pro-client.png"],
+  ["pro-createsession"],
+  ["signup.png"],
+];
+
+const content = contentArray.map(item => (
+  <div>
+    <img css={imageCss} src={images[item[0]]} />
+  </div>
+));
+
 const PhysIOCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
+  console.log(content);
+
   return (
-    <Carousel showThumbs={false} showStatus={false} style={{ height: "200px" }}>
-      <div>
-        <img src={images["about.png"]} />
-      </div>
-      <div>
-        <img src={images["client-dashboard.png"]} />
-      </div>
-      <div>
-        <img src={images["client-session.png"]} />
-      </div>
-      <div>
-        <img src={images["login.png"]} />
-      </div>
-      <div>
-        <img src={images["pro-client.png"]} />
-      </div>
-      <div>
-        <img src={images["pro-createsession.png"]} />
-      </div>
-      <div>
-        <img src={images["pro-dashboard.png"]} />
-      </div>
-      <div>
-        <img src={images["signup.png"]} />
-      </div>
-    </Carousel>
+    <Slider
+      {...settings}
+      css={css`
+      .slick-prev:before,
+    .slick-next:before {
+      color: grey;
+
+      .slick-slide {
+        position: relative;
+      }
+      `}
+    >
+      {content}
+    </Slider>
   );
 };
+
+{
+  /* <div>
+<div
+  css={css`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin-top: 5px;
+    padding-top: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  `}
+>
+  <div
+    css={css`
+      padding: 0 5px;
+      border-radius: 5px;
+      background: rgba(0, 0, 0, 0.3);
+    `}
+  >
+    Text
+  </div>
+</div>
+<img css={imageCss} src={images["about.png"]} />
+</div>
+<div>
+<img css={imageCss} src={images["client-dashboard.png"]} />
+<div
+  css={css`
+    padding: 10px 0;
+    display: flex;
+    justify-content: center;
+  `}
+>
+  <span>Testing dette....</span>
+</div>
+</div>
+<div>
+<img css={imageCss} src={images["client-session.png"]} />
+</div>
+<div>
+<img css={imageCss} src={images["login.png"]} />
+</div>
+<div>
+<img css={imageCss} src={images["pro-client.png"]} />
+</div>
+<div>
+<img css={imageCss} src={images["pro-createsession.png"]} />
+</div>
+<div>
+<img css={imageCss} src={images["pro-dashboard.png"]} />
+</div>
+<div>
+<img css={imageCss} src={images["signup.png"]} />
+</div> */
+}
 
 export default PhysIOCarousel;
