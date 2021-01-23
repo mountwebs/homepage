40b3b02914f33cdf37b9747a72915a6d2702effc
mountwebs <mountwebs@gmail.com>
2021-01-23 16:16:58 +0100

    styling github button

--- src/components/common/githubButton.js
+++ src/components/common/githubButton.js
@@ -4,7 +4,14 @@ import { SiGithub } from "react-icons/si";
 
 const GithubButton = ({ children, href, style }) => {
   return (
-    <div style={{ ...style, display: "inline", marginRight: "10px" }}>
+    <div
+      style={{
+        ...style,
+        display: "inline",
+        marginRight: "10px",
+        marginBottom: "10px",
+      }}
+    >
       <Button variant="contained" href={href} style={{ textTransform: "none" }}>
         <SiGithub
           style={{
